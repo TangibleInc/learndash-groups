8329625e495a750987df00edc3b30bfb75a9df88
eliot-akira <me@eliotakira.com>
2019-08-30 00:57:15 +0200

    Update build pipeline script and package.json

--- ld-groups.php
+++ ld-groups.php
@@ -3,7 +3,7 @@
  * Plugin Name: LearnDash Groups
  * Plugin URI: https://tangibleplugins.com
  * Description: Allow the use of Beaver Themer with LearnDash Student Groups and create pictures / banners fields connections for each student groups
- * Version: 0.0.0
+ * Version: 0.0.1
  * Author: Team Tangible
  * Author URI: https://teamtangible.com/
  * Text Domain: ld-groups
@@ -15,7 +15,7 @@
 defined( 'ABSPATH' ) or die( 'Nothing to see here' );
 
 
-define( 'TangibleGroups_VER', '0.0.0' );
+define( 'TangibleGroups_VER', '0.0.1' );
 define( 'TangibleGroups_FILE', __FILE__ );
 define( 'TangibleGroups_DIR', plugin_dir_path( __FILE__ ) );
 define( 'TangibleGroups_URL', plugins_url( '/', __FILE__ ) );
