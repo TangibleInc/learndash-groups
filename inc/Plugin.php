f732cacaf5d4c3ed0cd84e0ab6f3703a5ff135aa
eliot-akira <me@eliotakira.com>
2019-12-24 06:35:28 +0100

    Update plugin register method for framework

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Tangible\LDGroups;
 
@@ -13,136 +13,92 @@ use Tangible\PluginFramework;
 Class Plugin
 {
 
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
+  /**
+   * Constructor
+   */
+  public function __construct() {
 
-		// include plugin's functions
-		include __DIR__ . '/functions/index.php';
+    // include plugin's functions
+    include __DIR__ . '/functions/index.php';
 
-		// Initialize the tangible framework
-		$this->init_framework();
+    // Initialize the tangible framework
+    add_action( tangible_plugin_framework()->ready, [$this, 'init_framework'] );
 
-		// Handle the group access logic
-		new GroupAccess;
-		
-		// Handle the admin part of wordpress
-		new Admin;
+    // Handle the group access logic
+    new GroupAccess;
 
-		// Register field connections
-		new FieldConnections;
+    // Handle the admin part of wordpress
+    new Admin;
 
-		// Register all the shortcodes of the plugin
-		new Shortcodes;
+    // Register field connections
+    new FieldConnections;
 
-		// Get the settings
-		$this->settings = new Settings;
+    // Register all the shortcodes of the plugin
+    new Shortcodes;
 
-		// Register fileds
-		$this->register_fields();
-	}
-	
+    // Get the settings
+    $this->settings = new Settings;
 
-
-	/**
-	 * Register all the fields of the plugin
-	 */
-	private function register_fields() {
-
-		add_action( 'init', function() {
-			$this->settings->register_settings();
-		} );
-	}
+    // Register fileds
+    $this->register_fields();
+  }
 
 
 
-	/**
-	 * Use the Tangible internal plugin (see documentation)
-	 */
-	private function init_framework() {
+  /**
+   * Register all the fields of the plugin
+   */
+  private function register_fields() {
 
-		PluginFramework\register_plugin([
+    add_action( 'init', function() {
+      $this->settings->register_settings();
+    } );
+  }
 
-		  	// Required
-		  	'name' 				=> 'ld-groups',
-		  	'title' 			=> 'LearnDash Groups',
-		  	'setting_prefix' 	=> 'ldg',
-		  	'file_path' 		=> TangibleGroups_FILE,
 
-		  	// Must match with a product in the Tangible Plugins store
-		  	'item_id' 			=> 4945,
-		  	'version' 			=> TangibleGroups_VER,
 
-		  	// Plugin dependencies
-		  	'dependencies' 		=> [
-		    	'bb-plugin/fl-builder.php' => [
-		      		'title' => 'Beaver Builder',
-		      		'fallback_check' => function() { 
-		      			return class_exists( 'FLBuilder' ); 
-		      		}
-		    	],
-		    	'sfwd-lms/sfwd_lms.php' => [
-		      		'title' => 'LearnDash',
-		      		'fallback_check' => function() { 
-		      			return defined( 'LEARNDASH_VERSION' ); 
-		      		}
-		    	],
-			],
-		  	'missing_dependencies_message' => function( $plugin, $missing_deps ) {
+  /**
+   * Use the Tangible internal plugin (see documentation)
+   */
+  public function init_framework($framework) {
 
-		    	?><p><b>Missing plugin dependencies for </b></p><?php
+    $framework->register_plugin([
 
-		    	foreach( $missing_deps as $dep ) {
-		      	?><p><?= $dep['title'] ?></p><?php
-		    	}
-		  	},
+      // Required
+      'name'         => 'ld-groups',
+      'title'       => 'LearnDash Groups',
+      'setting_prefix'   => 'ldg',
+      'file_path'     => TangibleGroups_FILE,
 
-		  	// Settings page CSS/JS
-		  	'settings_css' 	=> plugins_url( 'settings/style.css', TangibleGroups_VER ),
-		  	'settings_js' 	=> plugins_url( 'settings/script.js', TangibleGroups_VER ),
+      // Must match with a product in the Tangible Plugins store
+      'item_id'       => 4945,
+      'version'       => TangibleGroups_VER,
 
-		  	// Title of settings page
-		  	'settings_title_callback' => function( $plugin, $tabs, $active_tab ) {
-		    	?><h2><?= $plugin['title'] ?></h2><?php
-		  	},
+      // Plugin dependencies
+      'dependencies'     => [
+        'sfwd-lms/sfwd_lms.php' => [
+            'title' => 'LearnDash',
+            'fallback_check' => function() {
+              return defined( 'LEARNDASH_VERSION' );
+            }
+        ],
+      ],
 
-		  	// Tabs in settings page
-		  	'setting_tabs' => [
-		    	'settings' => [
-		      		'title' 	=> __( 'Settings', 'ld-groups' ),
-		      		'callback' 	=> function () {
-						$this->settings->get_view();
-		      		},
-		    	],
-		  	],
+      // Settings page CSS/JS
+      'settings_css'   => plugins_url( 'settings/style.css', TangibleGroups_VER ),
+      'settings_js'   => plugins_url( 'settings/script.js', TangibleGroups_VER ),
 
-		  	// Action links shown in admin plugin list
-		  	'action_links' => [
-		    	'<a href="" target="_blank">Support</a>'
-		  	],
+      // Tabs in settings page
+      'setting_tabs' => [
+        'settings' => [
+          'title'   => __( 'Settings', 'ld-groups' ),
+          'callback'   => function () {
+            $this->settings->get_view();
+          },
+        ],
+      ],
+    ]);
 
-		  	// Admin notices - this also ensures support for multi-site
-		  	'admin_notice' => function ($plugin) {
-
-			    ?>
-			    	<div class="notice notice-info is-dismissible">
-			      		<p>Welcome to <?= $plugin['title'] ?>! Make sure to take a look at our list of recommended plugins.</p>
-			      		<p><a href="<?= PluginFramework\get_settings_page_url($plugin, 'recommend') ?>">See our recommendations &raquo;</a></p>
-			    	</div>
-			    <?php
-
-			    if (!PluginFramework\has_valid_license($plugin)) {
-			    
-			    	?>
-			      		<div class="notice notice-error">
-			        		<p>Please <a href="<?= PluginFramework\get_settings_page_url($plugin, 'license') ?>">enter your license key</a> to enable plugin updates for <?= $plugin['title'] ?>.</p>
-			      		</div>
-			      	<?php
-			    }
-		  	},
-		]);
-
-	}
+  }
 
 }
